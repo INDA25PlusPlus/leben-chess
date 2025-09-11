9d6a5cc4667d9e7c23a16a57ee4a48986efe5faf
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-09-11 18:23:50 +0200

    Add `Board` type with methods for getting and setting pieces, as well as creating new boards. Closes #4, closes #1

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug, Formatter};
 use crate::util::U3;
 
 use PieceType::*;
@@ -68,5 +69,95 @@ impl TryFrom<(u8, u8)> for BoardPosition {
     type Error = ();
     fn try_from(value: (u8, u8)) -> Result<Self, Self::Error> {
         Ok(BoardPosition { file: value.0.try_into()?, rank: value.1.try_into()? })
+    }
+}
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct Board { squares: [[Option<Piece>; 8]; 8] }
+
+impl Debug for Board {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for rank in (0u8..8).rev() {
+            write!(f, "\n{}", rank + 1)?;
+            for file in 0u8..8 {
+                let pos = BoardPosition { file: file.try_into().unwrap(), rank: rank.try_into().unwrap() };
+                let piece = self.get_piece(pos);
+                if let Some(piece) = piece {
+                    write!(f, " {}", piece.get_char())?;
+                } else {
+                    write!(f, "  ")?;
+                }
+            }
+        }
+        write!(f, "\n  a b c d e f g h")?;
+        Ok(())
+    }
+}
+
+impl Board {
+    const EMPTY_BOARD: Board = Board {
+        squares: [[None; 8]; 8]
+    };
+
+    const DEFAULT_BOARD: Board = Board {
+        squares: [
+            [
+                Some(Piece { piece_type: Rook, player: White }),
+                Some(Piece { piece_type: Knight, player: White }),
+                Some(Piece { piece_type: Bishop, player: White }),
+                Some(Piece { piece_type: Queen, player: White }),
+                Some(Piece { piece_type: King, player: White }),
+                Some(Piece { piece_type: Bishop, player: White }),
+                Some(Piece { piece_type: Knight, player: White }),
+                Some(Piece { piece_type: Rook, player: White }),
+            ],
+            [ Some(Piece { piece_type: Pawn, player: White }); 8 ],
+            [ None, None, None, None, None, None, None, None ],
+            [ None, None, None, None, None, None, None, None ],
+            [ None, None, None, None, None, None, None, None ],
+            [ None, None, None, None, None, None, None, None ],
+            [ Some(Piece { piece_type: Pawn, player: Black }); 8 ],
+            [
+                Some(Piece { piece_type: Rook, player: Black }),
+                Some(Piece { piece_type: Knight, player: Black }),
+                Some(Piece { piece_type: Bishop, player: Black }),
+                Some(Piece { piece_type: Queen, player: Black }),
+                Some(Piece { piece_type: King, player: Black }),
+                Some(Piece { piece_type: Bishop, player: Black }),
+                Some(Piece { piece_type: Knight, player: Black }),
+                Some(Piece { piece_type: Rook, player: Black }),
+            ]
+        ]
+    };
+
+    fn square_at(&self, pos: BoardPosition) -> &Option<Piece> {
+        &self.squares[pos.file.get() as usize][pos.rank.get() as usize]
+    }
+
+    fn square_at_mut(&mut self, pos: BoardPosition) -> &mut Option<Piece> {
+        &mut self.squares[pos.file.get() as usize][pos.rank.get() as usize]
+    }
+
+    pub fn get_piece(&self, pos: BoardPosition) -> Option<Piece> {
+        *self.square_at(pos)
+    }
+
+    pub fn set_piece(&mut self, pos: BoardPosition, piece: Option<Piece>) {
+        *self.square_at_mut(pos) = piece;
+    }
+
+    /// Instantiate a board from a 2D array of pieces, arranged first by file and then by rank
+    pub fn from_array(squares: [[Option<Piece>; 8]; 8]) -> Board {
+        Board { squares }
+    }
+
+    /// Instantiate an empty board
+    pub fn empty_board() -> Board {
+        Board::EMPTY_BOARD
+    }
+
+    /// Instantiate a board with the default chess piece configuration
+    pub fn default_board() -> Board {
+        Board::DEFAULT_BOARD
     }
 }
