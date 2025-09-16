88808719db49470c1fdb2dc39fae24e72c9681a5
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-09-16 22:49:37 +0200

    Add `ChessError` and basic `ChessGame` methods. Working towards #19, closes #17

--- src/chess.rs
+++ src/chess.rs
@@ -32,3 +32,92 @@ pub struct ChessGame {
     castling_rights: (CastlingRights, CastlingRights),
     en_passant_target: Option<BoardPosition>,
 }
+
+#[derive(Error, Debug)]
+pub enum ChessError {
+    #[error("game not started")]
+    GameNotStarted,
+    #[error("game has already ended")]
+    GameAlreadyEnded,
+    #[error("illegal move")]
+    IllegalMove,
+    #[error("it is the other player's turn")]
+    WrongTurn,
+    #[error("missing promotion type")]
+    MissingPromotionType,
+    #[error("expected `None` as promotion type: move is not a promotion move")]
+    UnexpectedPromotionType,
+}
+
+impl ChessGame {
+    pub fn new(starting_board: Board) -> ChessGame {
+        ChessGame {
+            game_status: GameStatus::NotYetStarted,
+            active_player: PlayerColor::White,
+            board: starting_board,
+            castling_rights: (CastlingRights::default(), CastlingRights::default()),
+            en_passant_target: None,
+        }
+    }
+
+    pub fn game_status(&self) -> &GameStatus {
+        &self.game_status
+    }
+
+    pub fn active_player(&self) -> PlayerColor {
+        self.active_player
+    }
+
+    pub fn board(&self) -> &Board {
+        &self.board
+    }
+
+    pub fn draw_by_agreement(&mut self) -> Result<(), ChessError> {
+        match self.game_status {
+            GameStatus::Normal => {
+                self.game_status = GameStatus::Draw(DrawReason::DrawByAgreement);
+                Ok(())
+            }
+            GameStatus::NotYetStarted => Err(ChessError::GameNotStarted),
+            GameStatus::Draw(..) | GameStatus::Win(..) => Err(ChessError::GameAlreadyEnded),
+        }
+    }
+
+    pub fn resign(&mut self, player: PlayerColor) -> Result<(), ChessError> {
+        match self.game_status {
+            GameStatus::Normal => {
+                if self.active_player == player {
+                    self.game_status = GameStatus::Win(player.other_player(),
+                                                       WinReason::Resignation);
+                    Ok(())
+                } else {
+                    Err(ChessError::WrongTurn)
+                }
+            }
+            GameStatus::NotYetStarted => Err(ChessError::GameNotStarted),
+            GameStatus::Draw(..) | GameStatus::Win(..) => Err(ChessError::GameAlreadyEnded),
+        }
+    }
+
+    pub fn active_piece(&self, pos: BoardPosition) -> bool {
+        if let Some(piece) = self.board.get_piece(pos) {
+            self.active_player == piece.player
+        } else {
+            false
+        }
+    }
+
+    fn castling_rights(&self, player: PlayerColor) -> CastlingRights {
+        match player {
+            PlayerColor::White => self.castling_rights.0,
+            PlayerColor::Black => self.castling_rights.1,
+        }
+    }
+
+    fn move_context(&self) -> MoveContext {
+        MoveContext {
+            castling_rights: self.castling_rights(self.active_player),
+            en_passant_target: self.en_passant_target,
+        }
+    }
+}
